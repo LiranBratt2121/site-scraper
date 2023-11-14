f4e719225918be6e0681f8825b09a151844d6553
LiranBratt2121 <brattlirannin@gmail.com>
2023-11-14 15:48:19 +0200

    site_parser: Update main to utilise text and image scarpping

--- site_parser.py
+++ site_parser.py
@@ -1,13 +1,22 @@
 from bs4 import BeautifulSoup
-from scrapper_utils import grab_pic
+from scrapper_utils import grab_img_url, download_img
+import asyncio
 
+def main():
+    with open('webpage.html', 'r', encoding="utf8") as file: 
+        soup = BeautifulSoup(file.read(), 'html.parser')
 
-with open('webpage.html', 'r', encoding="utf8") as file: 
-    soup = BeautifulSoup(file.read(), 'html.parser')
+        img = soup.find('img', class_='img_practice')
+        
+        text = [p.text[::-1] for p in soup.find_all('p')]
+        
+        print(text)
+        
+        img_url = grab_img_url(img)
 
-    content = soup.find('img', class_='img_practice')
-    
-    img_url = grab_pic(content)
-    
-    print("NO IMAGE" if not img_url else img_url)
-    
+        download_img(img_url, 'LIGMA')
+        
+        print("NO IMAGE" if not img_url else img_url)
+
+if __name__ == "__main__":
+    main()
