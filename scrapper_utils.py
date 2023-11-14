b77a18102addfa66ee87f11f9b4c7cd541e3e970
LiranBratt2121 <brattlirannin@gmail.com>
2023-11-14 15:47:32 +0200

    scrapper_utils: Add img download feature

--- scrapper_utils.py
+++ scrapper_utils.py
@@ -1,9 +1,22 @@
-def grab_pic(content):
+import urllib.request 
+import os
+
+def grab_img_url(content):
     try:
         return content.attrs['src']  
     except:
         print('No Image in the page.\nSkipped Adding a photo')
         
-        
+def download_img(url: str, name: str) -> None:
+    try:
+        os.mkdir('img')
+        print('Created IMG directory')
+    except:
+        print('Got DIR')
+
+    urllib.request.urlretrieve(url, f'img/{name}.jpg')
+    
+    print('Image Downloaded')
+            
 def grab_text():
     pass
